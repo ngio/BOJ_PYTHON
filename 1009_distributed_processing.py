f3c5aa414015ff7533268f581426581a68d86b61
wani <mill01@naver.com>
2023-02-17 12:09:10 +0900

    Update 1009_distributed_processing.py

--- 1009_distributed_processing.py
+++ 1009_distributed_processing.py
@@ -12,15 +12,38 @@
         각 테스트 케이스에 대해 마지막 데이터가 처리되는 컴퓨터의 번호를 출력한다.
 """
 
-
+"""
 t = int(input())
 
 for _ in range(t):
     a, b= map(int, input().split())
     print((a ** b)%10)
-    
+"""    
 
+       
+T = int(input())
+
+for _ in range(T):
+    a, b = map(int, input().split())
+    a = a % 10
     
+    if a == 0:
+        print(10)
+    elif a == 1 or a == 5 or a == 6:
+        print(a)
+    elif a == 4 or a == 9:
+        b = b % 2
+        if b == 1:
+            print(a)
+        else:
+            print((a * a) % 10)
+    else:
+        b = b % 4
+        if b == 0:
+            print((a**4) % 10 % 10 % 10)
+        else:
+            print((a**b) % 10 % 10 % 10)
+            
     
 """ OUTPUT
 >> 1009_distributed_processing.py
