9658d8f89a023861324624766e2038e088da64cd
wani <mill01@naver.com>
2023-02-21 22:28:10 +0900

    Update 1010_bridge.py

--- 1010_bridge.py
+++ 1010_bridge.py
@@ -38,5 +38,29 @@ for _ in range(T):
     bridge = math.factorial(m) // (math.factorial(n) * math.factorial(m - n))
     print(bridge)
     
+
+
+#-----------------------------------------------------------------------------
+
+import sys
+
+t = int(sys.stdin.readline())
+
+# 테스트 케이스만큼 반복
+for _ in range(t):
+    n, m = map(int, sys.stdin.readline().split())
+    a = m
+    b = n
+
+    # mCn 구현
+    for i in range(1, n):
+        a *= m - i # m!
+        b *= n - i # n!
+
+    print(a // b) # m! // n!
+    
+    
+    
+    
     
 
